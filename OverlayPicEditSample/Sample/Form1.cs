c30677bbd64c7ccd47dab3836d7118cf6fdb2e7c
Jacek Kashiash Kosiński <kashiash@gmail.com>
2019-06-01 13:58:52 +0200

    szczeka

---
+++ OverlayPicEditSample/Sample/Form1.cs
@@ -0,0 +1,76 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample {
+    public partial class Form1 : Form {
+        private MyPictureEdit pe;
+
+        public Form1() {
+            InitializeComponent();
+
+            pe = new MyPictureEdit();
+            this.Controls.Add(pe);
+
+            pe.LoadImage();
+
+            pe.Dock = System.Windows.Forms.DockStyle.Fill;
+            pe.BackColor = Color.Green;
+            pe.EditValue = global::Sample.Properties.Resources.Untitled;
+            pe.Location = new System.Drawing.Point(0, 0);
+            pe.Name = "pictureEdit1";
+            pe.Properties.ShowScrollBars = true;
+            pe.Properties.ShowZoomSubMenu = DevExpress.Utils.DefaultBoolean.True;
+            pe.Size = new System.Drawing.Size(292, 269);
+            pe.TabIndex = 0;
+            
+            pe.MouseDown += new System.Windows.Forms.MouseEventHandler(pe_MouseDown);
+            pe.MouseMove += new System.Windows.Forms.MouseEventHandler(pe_MouseMove);
+            pe.MouseUp += new System.Windows.Forms.MouseEventHandler(pe_MouseUp);
+            pe.Paint += new System.Windows.Forms.PaintEventHandler(pe_Paint);
+
+            //pe.ZoomPercentChanged += pe_ZoomPercentChanged;
+            //pe.MouseDown += pe_MouseDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Draw
+
+        // Start Rectangle
+        //
+        private void pe_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.pe.IsMouseDown(sender, e);
+        }
+
+        // Draw Rectangle
+        //
+        private void pe_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.pe.IsMouseMove(sender, e);
+        }
+
+        // Draw Area
+        //
+        private void pe_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            this.pe.ToPaint(sender, e);
+        }
+
+        private void pe_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.pe.IsMouseUp(sender, e);
+        }
+        #endregion
+        
+    }
+}
---
+++ OverlayPicEditSample/Sample/MyPictureEdit.cs
@@ -0,0 +1,213 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraEditors.Registrator;
+using System.Reflection;
+using DevExpress.XtraEditors.ViewInfo;
+using DevExpress.XtraEditors.Drawing;
+
+namespace Sample {
+    public class MyPictureEdit : PictureEdit {
+        int MouseWheelZoomIndex { get { return Math.Max(1, this.Properties.ZoomPercent / 10); } }
+        private PointF RectStartPoint;
+        private RectangleF Rect = new RectangleF();
+        private RectangleF[] Rects;
+        private Brush brush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+        private Pen pen = new System.Drawing.Pen(Color.FromArgb(0x48, 0x91, 0xDC));
+        bool isSpaceLocked = false;
+        private float originalPercent = 1;
+        double zoomImageX, zoomImageY;
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            this.Properties.ZoomPercent += Math.Sign(e.Delta) * MouseWheelZoomIndex;
+            base.OnMouseWheel(e);
+            Refresh();
+           if (Rect.Size != null && Rect.Location != null)
+                {
+                    float currentPercent = this.Properties.ZoomPercent;
+
+                    float relativePercent = currentPercent / originalPercent;
+
+                    float tempWidth = Rect.Size.Width;
+                    float tempHeight = Rect.Size.Height;
+                    Rect.Size = new SizeF(tempWidth * relativePercent, tempHeight * relativePercent);
+                    double zoomFactor = (double)Properties.ZoomPercent / 100;
+                    double deltaX = zoomImageX * ViewInfo.Image.Width * zoomFactor;
+                    double deltaY = zoomImageY * ViewInfo.Image.Height * zoomFactor;
+
+                    PropertyInfo pr = ViewInfo.GetType().GetProperty("HScrollBarPosition", BindingFlags.Instance | BindingFlags.NonPublic);
+                    int hScrollPosition = (int)pr.GetValue(ViewInfo, null);
+                    if (hScrollPosition >= 0)
+                        Rect.X = (int)(ViewInfo.PictureRect.X + deltaX - hScrollPosition);
+                    else
+                        Rect.X = (int)(ViewInfo.PictureStartX + deltaX);
+
+
+                    pr = ViewInfo.GetType().GetProperty("VScrollBarPosition", BindingFlags.Instance | BindingFlags.NonPublic);
+                    int vScrollPosition = (int)pr.GetValue(ViewInfo, null);
+                    if (vScrollPosition >= 0)
+                        Rect.Y = (int)(ViewInfo.PictureRect.Y + deltaY - vScrollPosition);
+                    else
+                        Rect.Y = (int)(ViewInfo.PictureStartY + deltaY);
+                    Debug.WriteLine("Begin");
+                    Debug.WriteLine(Rect);
+                    Debug.WriteLine(deltaX.ToString() + "    " + deltaY.ToString());
+                    Debug.WriteLine(zoomImageX.ToString() + "    " + zoomImageY.ToString());
+                    Debug.WriteLine(hScrollPosition.ToString() + "    " + vScrollPosition.ToString());
+                    Debug.WriteLine("End");
+                    Rects = new RectangleF[] { Rect };
+                    this.Invalidate();
+                    this.originalPercent = currentPercent;
+                }
+          
+
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (!isSpaceLocked || e.Button != MouseButtons.Left || !this.Properties.AllowScrollViaMouseDrag)
+            {
+                IsMouseDown(e);
+                return;
+            }
+            base.OnMouseDown(e);
+
+
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            isSpaceLocked = e.KeyCode == Keys.Space;
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            isSpaceLocked = !(e.KeyCode == Keys.Space);
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+
+        #region Draw
+
+        // Start Rectangle
+        //
+        public void IsMouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+
+            // Determine the initial rectangle coordinates...
+            RectStartPoint = e.Location;
+            double deltaX = RectStartPoint.X - ViewInfo.PictureStartX;
+            double deltaY = RectStartPoint.Y - ViewInfo.PictureStartY;
+            double zoomFactor =  (double)Properties.ZoomPercent / 100;
+            zoomImageX = deltaX / (ViewInfo.Image.Width * zoomFactor);
+            zoomImageY = deltaY / (ViewInfo.Image.Height * zoomFactor);
+           // new Rectangle(r.X + x, r.Y + y, (int)(vi.Image.Width * vi.Item.ZoomFactor), (int)(vi.Image.Height * vi.Item.ZoomFactor)
+            Invalidate();
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+        public void IsMouseDown(System.Windows.Forms.MouseEventArgs e)
+        {
+
+            // Determine the initial rectangle coordinates...
+            RectStartPoint = e.Location;
+            double deltaX, deltaY;
+            PropertyInfo pr = ViewInfo.GetType().GetProperty("HScrollBarPosition", BindingFlags.Instance | BindingFlags.NonPublic);
+            int hScrollPosition = (int)pr.GetValue(ViewInfo, null);
+            if (hScrollPosition >= 0)
+                deltaX = (int)(RectStartPoint.X - ViewInfo.PictureRect.X + hScrollPosition);
+            else
+                deltaX = RectStartPoint.X - ViewInfo.PictureStartX;
+
+            pr = ViewInfo.GetType().GetProperty("VScrollBarPosition", BindingFlags.Instance | BindingFlags.NonPublic);
+            int vScrollPosition = (int)pr.GetValue(ViewInfo, null);
+            if (vScrollPosition >= 0)
+                deltaY = (int)(RectStartPoint.Y - ViewInfo.PictureRect.Y + vScrollPosition);
+            else
+                deltaY = RectStartPoint.Y - ViewInfo.PictureStartY;
+            double zoomFactor = (double)Properties.ZoomPercent / 100;
+            zoomImageX = deltaX / (ViewInfo.Image.Width * zoomFactor);
+            zoomImageY = deltaY / (ViewInfo.Image.Height * zoomFactor);
+            Invalidate();
+            if (!isSpaceLocked)
+                this.Cursor = default(Cursor);
+        }
+
+        // Draw Rectangle
+        //
+        public void IsMouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left | isSpaceLocked)
+                return;
+            PointF tempEndPoint = e.Location;
+            Rect.Location = new PointF(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new SizeF(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+            Rects = new RectangleF[] { Rect };
+            originalPercent = this.Properties.ZoomPercent;
+            this.Invalidate();
+            this.Cursor = default(Cursor);
+
+            Debug.WriteLine(ViewInfo.PictureRect.ToString());
+            Debug.WriteLine(ViewInfo.PictureStartX.ToString());
+            Debug.WriteLine(ViewInfo.PictureStartY.ToString());
+            Debug.WriteLine(Rect.ToString());
+        }
+
+        // Draw Area
+        //
+        public void ToPaint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            if (isSpaceLocked)
+                return;
+            // Draw the rectangle...
+            if (this.Image != null)
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                    e.Graphics.DrawRectangles(pen, Rects);
+        }
+
+        public void ToPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            if (isSpaceLocked)
+                return;
+            // Draw the rectangle...
+            if (this.Image != null)
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                    e.Graphics.DrawRectangles(pen, Rects);
+        }
+
+        public void IsMouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (isSpaceLocked)
+                return;
+            if (e.Button == MouseButtons.Right)
+                if (Rect.Contains(e.Location))
+                    Debug.WriteLine("Right click");
+        }
+        #endregion
+    }
+
+}
